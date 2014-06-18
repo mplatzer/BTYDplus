13480688a97d29ab2fec938128af5a56362cecef
Michael Platzer <michael.platzer@microsoft.com>
2014-06-18 14:33:26 +0300

    added tests for HB models estimated by MCMC

---
+++ tests/testthat/test-pareto-cnbd-mcmc.r
@@ -0,0 +1,44 @@
+context("mle")
+
+test_that("Pareto/CNBD MCMC", {
+
+  # generate artificial Pareto/CNBD data 
+  set.seed(1)
+  params <- list(t=4.5, gamma=1.5, r=5, alpha=10, s=0.8, beta=12)
+  n <- 1000
+  cbs <- pcnbd.GenerateData(n, runif(n, 12, 96), 32, params, TRUE)$cbs
+  
+  # estimate parameters
+  draws <- pcnbd.mcmc.DrawParameters(cbs)
+  est <- as.list(summary(draws$level_2)$quantiles[, "50%"])
+  
+  expect_true(all(c("level_1", "level_2") %in% names(draws)))
+  expect_equal(length(draws$level_1), n)
+  expect_true(is.mcmc.list(draws$level_1[[1]]))
+  expect_true(is.mcmc.list(draws$level_2))
+  
+  # require less than 10% deviation in estimated parameters
+  ape <- function(act, est) abs(act-est)/act
+  expect_less_than(ape(params$r, est$r), 0.10)
+  expect_less_than(ape(params$alpha, est$alpha), 0.10)
+  expect_less_than(ape(params$s, est$s), 0.10)
+  expect_less_than(ape(params$beta, est$beta), 0.10)
+  expect_less_than(ape(params$t, est$t), 0.10)
+  expect_less_than(ape(params$gamma, est$gamma), 0.10)
+  
+  # estimate future transactions & P(alive)
+  xstar <- pcnbd.mcmc.DrawFutureTransactions(cbs, draws, T.star=cbs$T.star)
+  cbs$x.est <- apply(xstar, 2, mean)
+  cbs$pactive <- apply(xstar, 2, function(x) mean(x>0))
+  cbs$palive <- pcnbd.mcmc.PAlive(cbs, draws)
+
+  # require less than 5% deviation
+  expect_less_than(ape(sum(cbs$x.star), sum(cbs$x.est)), 0.05)
+  expect_less_than(ape(sum(cbs$palive), sum(cbs$alive)), 0.05)
+  expect_less_than(ape(sum(cbs$x.star>0), sum(cbs$pactive)), 0.05)
+
+  expect_true(min(cbs$x.star)>=0)
+  expect_true(all(cbs$x.star==round(cbs$x.star)))
+  expect_true(all(cbs$palive>=0 & cbs$palive<=1))
+  
+}
