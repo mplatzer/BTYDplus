0958ef9a48a236feeb422fbbe23386f8418db62b
mplatzer <michael.platzer@gmail.com>
2016-09-06 16:33:58 +0200

    relax check to allow for partial parameter naming

--- R/misc.R
+++ R/misc.R
@@ -2,13 +2,19 @@
 #' Wrapper for BTYD::dc.check.model.params with additional check for parameter
 #' names if these are present
 #'
+#' @param printnames Names to print parameter errors
+#' @param params model parameters
+#' @param func Function calling dc.check.model.params.safe
+#' @return stops program if there is something wrong with the parameters
 dc.check.model.params.safe <- function(printnames, params, func) {
   # first do basic checks
   dc.check.model.params(printnames, params, func)
   # then check for names, if these are present
-  if (!is.null(names(params)) && any(printnames != names(params)))
-    stop("Error in ", func, ": Parameter names do not match - ", 
-         paste0(printnames, collapse = ","), " != ",
-         paste0(names(params), collapse = ","), 
-         call. = FALSE)
+  if (!is.null(names(params))) {
+    idx <- names(params) != ""
+    if (any(printnames[idx] != names(params)[idx])) {
+      stop("Error in ", func, ": Parameter names do not match - ", paste0(printnames, collapse = ","), " != ", paste0(names(params), 
+        collapse = ","), call. = FALSE)
+    }
+  }
 }
--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -2,14 +2,17 @@
 context("misc")
 
 test_that("misc", {
-  
-  params <- c(k=1, r=2, alpha=3, a=4, 6)
-  printnames <- c("k", "r", "alpha", "a", "b")
-  expect_error(BTYDplus:::dc.check.model.params.safe(printnames, params, 'foo'))
 
   params <- c(k=1, r=2, alpha=3, a=4, b=6)
   printnames <- c("k", "r", "alpha", "a", "b")
   expect_silent(BTYDplus:::dc.check.model.params.safe(printnames, params, 'foo'))
+  
+  params <- c(k=1, r=2, alpha=3, a=4, 6)
+  printnames <- c("k", "r", "alpha", "a", "b")
+  expect_silent(BTYDplus:::dc.check.model.params.safe(printnames, params, 'foo'))
+
+  params <- c(k=1, r=2, alpha=3, x=4, b=6)
+  printnames <- c("k", "r", "alpha", "a", "b")
+  expect_error(BTYDplus:::dc.check.model.params.safe(printnames, params, 'foo'))
 
 })  
-
