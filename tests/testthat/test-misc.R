c17b88f2cd8412197cc5614c093e3e633f437d7a
mplatzer <michael.platzer@gmail.com>
2016-09-06 14:18:00 +0200

    non-exported methods are only accessible in tests by using three colons

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -5,11 +5,11 @@ test_that("misc", {
   
   params <- c(k=1, r=2, alpha=3, a=4, 6)
   printnames <- c("k", "r", "alpha", "a", "b")
-  expect_error(dc.check.model.params.safe(printnames, params, 'foo'))
+  expect_error(BTYDplus:::dc.check.model.params.safe(printnames, params, 'foo'))
 
   params <- c(k=1, r=2, alpha=3, a=4, b=6)
   printnames <- c("k", "r", "alpha", "a", "b")
-  expect_silent(dc.check.model.params.safe(printnames, params, 'foo'))
+  expect_silent(BTYDplus:::dc.check.model.params.safe(printnames, params, 'foo'))
 
 })  
 
