8e34c6ece3ce4a408466f44d5b4eabf96ef07937
mplatzer <michael.platzer@gmail.com>
2016-09-23 09:06:44 +0200

    report range of elog dates in demos

--- demo/pareto-abe.R
+++ demo/pareto-abe.R
@@ -1,9 +1,11 @@
 
 #' Load transaction records of 2357 CDNow customers.
-cdnowElog <- read.csv(system.file("data/cdnowElog.csv", package = "BTYD"), 
-                      stringsAsFactors = FALSE, 
+cdnowElog <- read.csv(system.file("data/cdnowElog.csv", package = "BTYD"),
+                      stringsAsFactors = FALSE,
                       col.names = c("cust", "sampleid", "date", "cds", "sales"))
 cdnowElog$date <- as.Date(as.character(cdnowElog$date), format = "%Y%m%d")
+head(cdnowElog)
+range(cdnowElog$date)
 
 #' Convert from event log to customer-by-sufficient-statistic summary.
 #' Split into 39 weeks calibration, and 39 weeks holdout period.
@@ -14,8 +16,8 @@ x <- readline("Estimate Pareto/NBD Abe without covariates (press Enter)")
 
 #' Estimate with no covariates; see model M1 in Abe (2009)
 draws.m1 <- abe.mcmc.DrawParameters(
-              cal.cbs = cbs, 
-              mcmc = 5000, burnin = 5000, 
+              cal.cbs = cbs,
+              mcmc = 5000, burnin = 5000,
               mc.cores = 1)
 round(summary(draws.m1$level_2)$quantiles[, c("2.5%", "50%", "97.5%")], 2)
 #' -> Parameter Estimates match Table 3 in Abe (2009);
@@ -30,12 +32,12 @@ cbs <- merge(cbs, first, by = "cust")
 
 #' Estimate with first purchase spend as covariate; see model M2 in Abe (2009)
 draws.m2 <- abe.mcmc.DrawParameters(
-              cal.cbs = cbs, 
-              covariates = c("first.sales"), 
-              mcmc = 5000, burnin = 5000, 
+              cal.cbs = cbs,
+              covariates = c("first.sales"),
+              mcmc = 5000, burnin = 5000,
               mc.cores = 1)
 round(summary(draws.m2$level_2)$quantiles[, c("2.5%", "50%", "97.5%")], 4)
-#' -> Parameter Estimates match Table 3 in Abe (2009), except for 
+#' -> Parameter Estimates match Table 3 in Abe (2009), except for
 #' `log_lambda_first` and `log_mu_first`; note however, that via simulation we
 #' can establish that our implementation is able to re-identify the underlying
 #' parameters correctly; see `tests/testthat/test-pareto-nbd-abe.R`
@@ -54,5 +56,5 @@ cbs$xstar.m1 <- apply(xstar.m1.draws, 2, mean)
 cbs$xstar.m2 <- apply(xstar.m2.draws, 2, mean)
 
 #' 3) compare mean absolute error at individual level
-round(c(`MAE without covariates` = mean(abs(cbs$x.star - cbs$xstar.m1)), 
+round(c(`MAE without covariates` = mean(abs(cbs$x.star - cbs$xstar.m1)),
         `MAE with covariates`    = mean(abs(cbs$x.star - cbs$xstar.m2))), 4)
